bef989affc6eb5defe091ef03ec6510d55ad8031
bleso-a <blessingadesiji96@gmail.com>
2021-06-04 00:57:33 +0100

    implements bottom tab bar navigation

---
+++ screens/MainLayout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {
+    View
+} from 'react-native';
+
+import { COLORS, SIZES, icons} from '../constants';
+
+const MainLayout = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default MainLayout;
